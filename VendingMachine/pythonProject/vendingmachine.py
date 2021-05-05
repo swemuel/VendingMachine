c8fa3932a31e13cab0f9b1eafe20b6e689379459
swemuel <swemoulson@gmail.com>
2021-05-06 11:34:07 +1200

    Stuck trying to get output to console on web

---
+++ VendingMachine/pythonProject/vendingmachine.py
@@ -0,0 +1,4 @@
+class VendingMachine:
+    def __init__(self, newName, newLocation):
+        self.name = newName
+        self.location = newLocation
--- VendingMachine/pythonProject/webserver.py
+++ VendingMachine/pythonProject/webserver.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
+from controller import setup
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    theVendingMachine = setup()
+    return render_template('index.html')
